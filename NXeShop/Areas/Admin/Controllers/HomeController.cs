f07a95e1950ae9a98f3fd027b33acc5e78a96696
ThaiNguyenDev <tranthainguyenttn99@gmail.com>
2023-03-29 16:33:03 +0700

    add comment

--- NXeShop/Areas/Admin/Controllers/HomeController.cs
+++ NXeShop/Areas/Admin/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace NXeShop.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        // GET: Admin/Home/Index
         public IActionResult Index()
         {
             return View();
