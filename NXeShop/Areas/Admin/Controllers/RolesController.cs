dc9e827d88bcef63ccb08c24558a4bf7449c6924
DELL <tranthainguyen10git config --global user.email tranthainguyen10@gmail.com>
2023-03-29 16:30:50 +0700

    Add project files.

---
+++ NXeShop/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NXeShop.Models;
+
+namespace NXeShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly EWEBBANHANGDBDBMARKETSMDFContext _context;
+
+        public RolesController(EWEBBANHANGDBDBMARKETSMDFContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Roles.ToListAsync());
+        }
+
+        // GET: Admin/Roles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
+            var role = await _context.Roles.FirstOrDefaultAsync(r => r.RoleId == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Admin/Roles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoleId,RoleName,Description")] Role role)
+        {
+            if (ModelState.IsValid) { 
+                _context.Roles.Add(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Admin/Roles/Edit?id =
+        public async Task<IActionResult> Edit(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Admin/Roles/Edit?id=
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoleId,RoleName,Description")] Role role)
+        {
+            if (id != role.RoleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(role);
+                    await _context.SaveChangesAsync();
+                }
+                catch(Exception ex)
+                {
+                   Console.WriteLine(ex.ToString());
+                }
+
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Admin/Roles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles.FirstOrDefaultAsync(m => m.RoleId == id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: Admin/Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        }
+}
---
+++ NXeShop/Program.cs
@@ -0,0 +1,54 @@
+using AspNetCoreHero.ToastNotification;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using NXeShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Add AddRazorRuntimeCompilation 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+var connectionString = builder.Configuration.GetConnectionString("dbNXeShop");
+builder.Services.AddDbContext<EWEBBANHANGDBDBMARKETSMDFContext>(x => x.UseSqlServer(connectionString));
+
+// Add NotyF
+builder.Services.AddNotyf(config => 
+                            { config.DurationInSeconds = 10; 
+                                config.IsDismissable = true; 
+                                config.Position = NotyfPosition.BottomRight; });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseHttpLogging();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    // admin
+    endpoints.MapControllerRoute(
+                 name: "areas",
+                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+               );
+    // user
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+});
+
+app.Run();
