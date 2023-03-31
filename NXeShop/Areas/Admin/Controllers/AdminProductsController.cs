78536eb53c3d15c7c5c47d9dc526bfd0c3d5b318
ThaiNguyenDev <tranthainguyenttn99@gmail.com>
2023-03-31 15:09:37 +0700

    Add AdminProducts handler and update source

---
+++ NXeShop/Areas/Admin/Controllers/AdminProductsController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NXeShop.Models;
+using PagedList.Core;
+
+namespace NXeShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminProductsController : Controller
+    {
+        private readonly EWEBBANHANGDBDBMARKETSMDFContext _context;
+
+        public AdminProductsController(EWEBBANHANGDBDBMARKETSMDFContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminProducts
+        public IActionResult Index(int? page, int CatId = 0)
+        {
+            // phan trang
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            int pageSize = 10;
+
+            List<Product> lstProducts = new List<Product>();
+            if (0 == CatId)
+            {
+                lstProducts = _context.Products
+                                .AsNoTracking()
+                                .Include(c => c.Cat)
+                                .OrderByDescending(x => x.ProductId)
+                                .ToList();
+            }
+            else
+            {
+                lstProducts = _context.Products
+                                .AsNoTracking()
+                                .Where(s => s.CatId == CatId)
+                                .Include(c => c.Cat)
+                                .OrderByDescending(x => x.ProductId)
+                                .ToList();
+            }
+
+            PagedList<Product> models = new PagedList<Product>(lstProducts.AsQueryable(), pageNumber, pageSize);
+
+            ViewData["DanhMuc"] = new SelectList(_context.Categories,"CatId", "CatName", CatId);
+
+
+            ViewBag.CurrentPage = pageNumber;
+            ViewBag.CurentCatID = CatId;
+            return View(models);
+        }
+
+        //Admin/AdminProducts/Fillter
+        public IActionResult Fillter(int CatId = 0)
+        {
+            var url = $"/Admin/AdminProducts?CatId={CatId}";
+            if (CatId == 0)
+            {
+                url = $"/Admin/AdminProducts";
+            }
+
+            return Json(new { status = "success", redirectUrl = url });
+        }
+
+        // GET: Admin/AdminProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId");
+            return View();
+        }
+
+        // POST: Admin/AdminProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ShortDesc,Description,CatId,Price,Discount,Thumb,Video,DateCreated,DateModified,BestSellers,HomeFlag,Active,Tags,Title,Alias,MetaDesc,MetaKey,UnitsInStock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            return View(product);
+        }
+
+        // POST: Admin/AdminProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ShortDesc,Description,CatId,Price,Discount,Thumb,Video,DateCreated,DateModified,BestSellers,HomeFlag,Active,Tags,Title,Alias,MetaDesc,MetaKey,UnitsInStock")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CatId"] = new SelectList(_context.Categories, "CatId", "CatId", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/AdminProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/AdminProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'EWEBBANHANGDBDBMARKETSMDFContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Products?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}
